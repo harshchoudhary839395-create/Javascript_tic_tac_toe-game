ed312b5c65e94b4a7e6711e84ea266cc10177730
Harsh Choudhary <harshchoudhary839395@gmail.com>
2026-02-22 15:02:33 +0530

    feature: draw situation

--- tic.js
+++ tic.js
@@ -4,7 +4,8 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turn0 =true;
+let turnO = true;
+let moveCount = 0;
 
 const winPatterns = [
     [0,1,2],
@@ -16,59 +17,78 @@ const winPatterns = [
     [3,4,5],
     [6,7,8],
 ];
+
 boxes.forEach((box) => {
-    box.addEventListener("click",() => {
-        console.log("box was clicked");
-        if (turn0){
-            box.innerText = "0";
-            turn0=false;
+    box.addEventListener("click", () => {
+
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
         } else {
             box.innerText = "X";
-            turn0 = true;
+            turnO = true;
         }
+
         box.disabled = true;
+        moveCount++;
+
         checkWinner();
-    }); 
+    });
 });
 
+const disableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+};
+
 const enableBoxes = () => {
-    for (let box of boxes){
+    boxes.forEach((box) => {
         box.disabled = false;
         box.innerText = "";
-    }
-} 
+    });
+};
+
 const showWinner = (winner) => {
     msg.innerText = `Congratulations, Winner is ${winner}`;
     msgContainer.classList.remove("hide");
-    
-}
- 
+    disableBoxes();
+};
+
+const showDraw = () => {
+    msg.innerText = "Game is a Draw!";
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
 const checkWinner = () => {
+
     for (let pattern of winPatterns) {
+
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if (pos1Val!="" && pos2Val!="" && pos3Val!="" ) {
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
-            console.log("winner", pos1Val);
-            showWinner( pos1Val);
-            count();
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+                return;
             }
         }
+    }
+
+    // Draw Condition
+    if (moveCount === 9) {
+        showDraw();
     }
 };
 
 const resetGame = () => {
-    turn0 = true;
+    turnO = true;
+    moveCount = 0;
     enableBoxes();
     msgContainer.classList.add("hide");
-}
+};
+
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
-
-const count = (draw) => {
-    if(count==9){
-        msg.innerText = `Match is Draw ${draw}`;
-    }
-}
